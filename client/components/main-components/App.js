7b3c640da80af03064cdcd007935042eb9170808
ace11 <ace11.space22@gmail.com>
2019-03-01 16:08:37 +0100

    client: added font-awesome

--- client/components/main-components/App.js
+++ client/components/main-components/App.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage, faFolder } from '@fortawesome/free-solid-svg-icons';
 
 import Home from './Home';
 import Book from './Book';
 import Header from './Header';
 import Footer from './Footer';
 import NotFound from './NotFound';
+
+library.add(fab, faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage, faFolder );
 
 const App = () => 
   <Fragment>
