43c04482e835fc9b5ad1689d520c446876a8a8c3
ace11 <ace11.space22@gmail.com>
2019-03-01 16:09:14 +0100

    Footer.js

--- client/components/main-components/Footer.js
+++ client/components/main-components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Footer = () => 
-  <footer>
-    this is a footer
-  </footer>
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <address>created by ace11</address>
+      <ul className='social-media'>
+        <li><a href='https://github.com/ace1122sp' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={['fab', 'github']} /> github | </a></li>
+        <li><a href='https://www.linkedin.com/in/aleksandar-bulovi%C4%87-83aa74139/' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin']} /> linkedin | </a></li>
+        <li><a href='https://ace1122sp.github.io/portfolio/' rel='noreferrer' target='_blank'><FontAwesomeIcon icon='folder' /> portfolio </a></li>
+      </ul>
+    </footer>
+  );
+}
 
 export default Footer;
