6d209dfecc613250302b55308303c13bf9e5ded2
ace11 <ace11.space22@gmail.com>
2019-03-02 21:21:12 +0100

    client:BookCard modifications

--- client/components/helper-components/BookCard.js
+++ client/components/helper-components/BookCard.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const BookCard = ({ title, commentcount }) => 
   <li>
-    <b>{title}</b> <i>{commentcount} {commentcount == 1 ? 'comment' : 'comments'}</i>
+    <b>{title}</b>
+    <i>{commentcount} {commentcount == 1 ? 'comment' : 'comments'}</i>
   </li>
 
 export default BookCard;
