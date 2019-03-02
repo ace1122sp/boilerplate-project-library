145cf73a174e3a81989703c651be116db1d21a0b
ace11 <ace11.space22@gmail.com>
2019-03-02 17:46:17 +0100

    client libs: added fetchNewBook()

--- client/libs/api-caller.js
+++ client/libs/api-caller.js
@@ -5,5 +5,21 @@ export const fetchBooks = url => {
       return res.json();
     })
     .then(res => res)
-    .catch(err => {}) // to handle
+    .catch(err => {}); // to handle
+};
+
+export const fetchNewBook = (url, title) => {
+  const initObject = { // think about adding more headers
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title })
+  };
+
+  return fetch(url, initObject)
+    .then(res => {
+      if (!res.ok) return Promise.reject(res.status); // to handle
+      return res.json();
+    })
+    .then(res => res)
+    .catch(err => {}); // to handle
 };
