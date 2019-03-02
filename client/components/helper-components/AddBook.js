6505ddb68a8fa6cf2603dc7f88714e07f83ad332
ace11 <ace11.space22@gmail.com>
2019-03-02 17:46:47 +0100

    client: AddBook component fully functional

--- client/components/helper-components/AddBook.js
+++ client/components/helper-components/AddBook.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchNewBook } from '../../libs/api-caller';
+import { API_BASE } from '../../constants';
 
-const AddBook = ({ close }) => 
-  <div>
-    <button onClick={close}>x</button>
-    <form>
-      <label>Book Title</label>
-      <input type='text' placeholder='new book' autoFocus />
-      <button>add</button>
-    </form>
-  </div>
+const AddBook = ({ close }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetchNewBook(API_BASE, inputValue)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {}); // to handle
+    setInputValue('');
+  }
+
+  const handleChange = e => {
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <div>
+      <button onClick={close}>x</button>
+      <form onSubmit={handleSubmit}>
+        <label>Book Title</label>
+        <input type='text' placeholder='new book' autoFocus onChange={handleChange} value={inputValue} />
+        <button>add</button>
+      </form>
+    </div>
+  );
+}  
 
 export default AddBook;
