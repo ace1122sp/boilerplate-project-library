7bf1381e8f85ee6ce16340ba6a068d52ce1c6e74
ace11 <ace11.space22@gmail.com>
2019-03-07 15:54:39 +0100

    styling: updated _reset.scss; Header component

--- client/components/main-components/Header.js
+++ client/components/main-components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../../css/Header.scss';
+
 const Header = () => 
-  <header>
+  <header className='header'>
     <Link to="/">Library fCC</Link>
   </header>
 
