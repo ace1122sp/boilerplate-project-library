a6958b24d5dae28b3755cd56dec641c394b1301e
ace11 <ace11.space22@gmail.com>
2019-03-09 12:47:32 +0100

    style: LoadingPanel

--- client/components/helper-components/LoadingPanel.js
+++ client/components/helper-components/LoadingPanel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import '../../css/LoadingPanel.scss';
+
 const LoadingPanel = () => 
     <div className='loading-panel'>
       <span><FontAwesomeIcon icon='spinner' size='3x' className='fa-spin fa-pulse' /></span>
